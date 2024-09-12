e56ff7158e7ee634544debd05bc863366f8eb548
aymaneallaoui <aymanepraxe@gmail.com>
2024-09-12 17:37:34 +0100

    fixing stuff

--- zod/errors.go
+++ zod/errors.go
@@ -5,25 +5,21 @@ import (
 	"fmt"
 )
 
-// ValidationError stores details about validation failures.
 type ValidationError struct {
 	Field   string      `json:"field"`
 	Value   interface{} `json:"value"`
 	Message string      `json:"message"`
 }
 
-// Error formats the error in plain text.
 func (e *ValidationError) Error() string {
 	return fmt.Sprintf("Validation error on field '%s': %s (Value: %v)", e.Field, e.Message, e.Value)
 }
 
-// ErrorJSON formats the error as a JSON string.
 func (e *ValidationError) ErrorJSON() string {
 	jsonData, _ := json.Marshal(e)
 	return string(jsonData)
 }
 
-// NewValidationError creates a new validation error for a specific field.
 func NewValidationError(field string, value interface{}, message string) *ValidationError {
 	return &ValidationError{
 		Field:   field,
--- zod/validators/stucts.go
+++ zod/validators/stucts.go
@@ -10,10 +10,9 @@ import (
 type ObjectSchema struct {
 	fields   map[string]zod.Schema
 	required bool
-	defaults map[string]interface{} // Default values
+	defaults map[string]interface{}
 }
 
-// Object returns a schema for an object (similar to struct in Go)
 func Object(fields map[string]zod.Schema) *ObjectSchema {
 	return &ObjectSchema{
 		fields:   fields,
@@ -26,13 +25,11 @@ func (o *ObjectSchema) Required() *ObjectSchema {
 	return o
 }
 
-// Default sets a default value for a field.
 func (o *ObjectSchema) Default(field string, value interface{}) *ObjectSchema {
 	o.defaults[field] = value
 	return o
 }
 
-// Validate uses goroutines to parallelize the validation of fields in nested structures.
 func (o *ObjectSchema) Validate(data interface{}) error {
 	obj, ok := data.(map[string]interface{})
 	if !ok {
@@ -49,7 +46,6 @@ func (o *ObjectSchema) Validate(data interface{}) error {
 		go func(k string, v interface{}, s zod.Schema) {
 			defer wg.Done()
 
-			// If the field doesn't exist and a default value is defined, assign the default.
 			if !exists {
 				if defaultValue, hasDefault := o.defaults[k]; hasDefault {
 					obj[k] = defaultValue
@@ -60,24 +56,21 @@ func (o *ObjectSchema) Validate(data interface{}) error {
 				}
 			}
 
-			// Validate the nested schema.
 			if err := s.Validate(v); err != nil {
 				errChan <- zod.NewValidationError(k, v, err.Error())
 			}
 		}(key, value, schema)
 	}
 
-	// Close the error channel after all goroutines are done.
 	go func() {
 		wg.Wait()
 		close(errChan)
 	}()
 
-	// Collect errors.
 	var combinedErrors []string
 	for err := range errChan {
 		if err != nil {
-			// If it's a validation error, convert to JSON or string.
+
 			combinedErrors = append(combinedErrors, err.(*zod.ValidationError).ErrorJSON())
 		}
 	}
